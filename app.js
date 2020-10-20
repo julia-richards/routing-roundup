2da747cbc5fe403b688a3849cc0fe12c8a56cd21
Kerri <gant.kerri@gmail.com>
2020-10-20 18:28:27 -0400

    finished phase four

--- app.js
+++ app.js
@@ -4,23 +4,28 @@ const app = express();
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-	res.send("Hello from Express!");
+    res.send("Hello from Express!");
 });
 
 app.get('/*xyz', (req, res) => {
     res.send("That's all I wrote.");
 });
 
-app.get(/\/*/i, (req, res) => {
-	const method = req.method;
-	const path = req.path;
-	const randomNumber = Math.floor(Math.random() * 100);
+app.get("^/capital-letters/:text", (req, res) => {
+    const string = req.params.text;
+    res.send(string.toUpperCase());
+})
 
-	res.render("layout", { method, path, randomNumber });
+app.get(/\/*/i, (req, res) => {
+    const method = req.method;
+    const path = req.path;
+    const randomNumber = Math.floor(Math.random() * 100);
+
+    res.render("layout", { method, path, randomNumber });
 });
 
 
 const port = 8081;
 app.listen(port, () => {
-	console.log(`Listening on port ${port}...`);
+    console.log(`Listening on port ${port}...`);
 });

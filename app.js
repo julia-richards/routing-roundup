14fcc11311384ea27cac2d2afca4ff4c64b9f9c3
julia-richards <julia.m.richards@gmail.com>
2020-10-20 15:41:55 -0600

    troubleshooting regex for xyz path

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.set('view engine', 'pug')
-
+app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-    res.send("Hello from Express!");
+	res.send("Hello from Express!");
+});
+
+app.get(.endsWith('xyz'), (req, res) => {
+    res.send("That's all I wrote.");
 });
 
 app.get(/\/*/i, (req, res) => {
-    const method = req.method;
-    const path = req.path;
-    const randomNumber = Math.floor(Math.random() * 100);
+	const method = req.method;
+	const path = req.path;
+	const randomNumber = Math.floor(Math.random() * 100);
 
-
-    res.render('layout', { method, path, randomNumber })
+	res.render("layout", { method, path, randomNumber });
 });
 
+
 const port = 8081;
-app.listen(port, () => { console.log(`Listening on port ${port}...`) });
+app.listen(port, () => {
+	console.log(`Listening on port ${port}...`);
+});
